f53fe0f88366bebcade2518357e2bb5e9867b9ea
Brutus5000 <Brutus5000@gmx.net>
2023-01-15 09:43:33 +0100

    Fix mongodb post-install script

--- charts/mongodb/scripts/create-db-and-users.sh
+++ charts/mongodb/scripts/create-db-and-users.sh
@@ -3,26 +3,26 @@
 # Fail on error
 set -e
 
-# Login to MongoDB
-mongosh --host mongodb-0 -u root -p "${MONGO_INITDB_ROOT_PASSWORD}"
+# Set alias with shell login to MongoDB
+alias mongo='mongosh --host mongodb -u ${MONGODB_ROOT_USER} -p "${MONGO_INITDB_ROOT_PASSWORD}" --quiet'
 
 # Check if the database exists
-database_exists=$(mongo --eval "print(db.getMongo().getDBNames().indexOf('my_database') != -1)" 2>/dev/null)
+database_exists=$(mongo --eval "\"print(db.getMongo().getDBNames().indexOf('${NODEBB_DATABASE}') != -1)\"" 2>/dev/null)
 
 # If the database does not exist, create it
 if [ "$database_exists" = "false" ]; then
-    mongosh --host mongodb-0 --eval "db.getSiblingDB('${NODEBB_DATABASE}').createCollection('dummy_collection')"
+    mongo --eval "db.getSiblingDB('${NODEBB_DATABASE}').createCollection('${NODEBB_DATABASE}')"
     echo "Database '${NODEBB_DATABASE}' has been created."
 else
     echo "Database '${NODEBB_DATABASE}' already exists."
 fi
 
 # Check if the user exists
-user_exists=$(mongosh --host mongodb-0 --eval "db.getUsers().forEach(function(user) { if (user.user == '${NODEBB_USERNAME}') { print(true); } });" my_database 2>/dev/null)
+user_exists=$(mongo -eval "db.getUsers().users.some (user => user.user == 'nodebb') " admin)
 
 # If the user does not exist, add it and grant it permissions to the database
-if [ "$user_exists" = "" ]; then
-    mongosh  --host mongodb-0 ${NODEBB_DATABASE} --eval "db.createUser({ user: '${NODEBB_USERNAME}', pwd: '${NODEBB_PASSWORD}', roles: [{ role: 'readWrite', db: '${NODEBB_DATABASE}' }] })"
+if [ "$user_exists" = "false" ]; then
+    mongo --eval "\"db.createUser({ user: '${NODEBB_USERNAME}', pwd: '${NODEBB_PASSWORD}', roles: [{ role: 'readWrite', db: '${NODEBB_DATABASE}' }] })\"" admin
     echo "User '${NODEBB_USERNAME}' has been added to database '${NODEBB_DATABASE}' with read-write permissions."
 else
     echo "User '${NODEBB_USERNAME}' already exists in database '${NODEBB_DATABASE}'."
